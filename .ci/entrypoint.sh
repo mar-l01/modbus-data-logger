8bf3d2d5343347eef4c2853eb5d16108f5446713
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-05-06 19:21:27 +0200

    provide entrypoint.sh file

---
+++ .ci/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# stop execution upon errors
+set -e
+
+# obtain scriptname
+SCRIPT=$1
+
+# do not allow an empty scriptname
+if [ -z "${SCRIPT}" ]; then
+    echo "A script to execute needs to be provided.."
+    exit 1
+fi
+
+# check if script exists
+if [ ! -e "${script}" ]; then
+    echo "The provided script '${SCRIPT}' does not exist.."
+    exit 1
+fi
+
+# if not already done, make sure script can be executed
+chmod 755 "${SCRIPT}"
+
+# run the script
+${script}
+
+# in case of an error we wouldn't have reached this line
+exit 0
