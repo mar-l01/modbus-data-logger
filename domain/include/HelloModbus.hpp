62c34a5ce53ce5817220be0b083694ef0b11ce6a
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-05-06 12:21:13 +0200

    introduced clang-tidy

--- domain/include/HelloModbus.hpp
+++ domain/include/HelloModbus.hpp
@@ -4,7 +4,7 @@
 class HelloModbus
 {
 public:
-    HelloModbus(const std::string& text);
+    explicit HelloModbus(const std::string& text);
     std::string getModbusText();
 
 private:
--- scripts/build_native.sh
+++ scripts/build_native.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIRECTORY="$PWD/../builds/build_native"
+DIRECTORY="$PWD/builds/build_native"
 
 # create build directory if it is not present
 if [ ! -d "$DIRECTORY" ]; then
@@ -12,7 +12,7 @@ fi
 cd $DIRECTORY
 
 # set up ninja building for cmake
-cmake -GNinja ../..
+cmake -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../..
 
 # build project using ninja
 cd $DIRECTORY
