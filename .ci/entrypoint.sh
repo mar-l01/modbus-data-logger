890eae6b5230e0f727291c5aed672fb20b1587f3
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-05-07 11:41:34 +0200

    yet another script-path

--- .ci/entrypoint.sh
+++ .ci/entrypoint.sh
@@ -6,34 +6,26 @@ set -e
 # obtain scriptname
 SCRIPT=$1
 
-echo $GITHUB_WORKSPACE
-SCRIPT_PATH=`realpath $0`
-SCRIPT_DIR=`dirname $SCRIPT_PATH`
-echo $SCRIPT_PATH
-echo $SCRIPT_DIR
-
-BUILD_SCRIPT_DIR="$SCRIPT_DIR/../.ci"
-
-EXE_SCRIPT="$BUILD_SCRIPT_DIR/SCRIPT"
-
+# build absolute path
+SCRIPT_PATH="${GITHUB_WORKSPACE}/${SCRIPT}"
 
 # do not allow an empty scriptname
-if [ -z "${EXE_SCRIPT}" ]; then
+if [ -z "${SCRIPT_PATH}" ]; then
     echo "A script to execute needs to be provided.."
     exit 1
 fi
 
 # check if script exists
-if [ ! -e "${EXE_SCRIPT}" ]; then
-    echo "The provided script '${EXE_SCRIPT}' does not exist.."
+if [ ! -e "${SCRIPT_PATH}" ]; then
+    echo "The provided script '${SCRIPT_PATH}' does not exist.."
     exit 1
 fi
 
 # if not already done, make sure script can be executed
-chmod 755 "${EXE_SCRIPT}"
+chmod 755 "${SCRIPT_PATH}"
 
 # run the script
-./${EXE_SCRIPT}
+${SCRIPT_PATH}
 
 # in case of an error we wouldn't have reached this line
 exit 0
