2a50d60c4a12b748026ea34a69b3be759661d93f
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-05-07 11:35:51 +0200

    test test test..

--- .ci/entrypoint.sh
+++ .ci/entrypoint.sh
@@ -3,30 +3,37 @@
 # stop execution upon errors
 set -e
 
-echo $PWD
-echo $GITHUB_WORKSPACE
-echo `realpath $0`
-
 # obtain scriptname
 SCRIPT=$1
 
+echo $GITHUB_WORKSPACE
+SCRIPT_PATH=`realpath $0`
+SCRIPT_DIR=`dirname $SCRIPT_PATH`
+echo $SCRIPT_PATH
+echo $SCRIPT_DIR
+
+BUILD_SCRIPT_DIR="$SCRIPT_DIR/../.ci"
+
+EXE_SCRIPT="$BUILD_SCRIPT_DIR/SCRIPT"
+
+
 # do not allow an empty scriptname
-if [ -z "${SCRIPT}" ]; then
+if [ -z "${EXE_SCRIPT}" ]; then
     echo "A script to execute needs to be provided.."
     exit 1
 fi
 
 # check if script exists
-if [ ! -e "${SCRIPT}" ]; then
-    echo "The provided script '${SCRIPT}' does not exist.."
+if [ ! -e "${EXE_SCRIPT}" ]; then
+    echo "The provided script '${EXE_SCRIPT}' does not exist.."
     exit 1
 fi
 
 # if not already done, make sure script can be executed
-chmod 755 "${SCRIPT}"
+chmod 755 "${EXE_SCRIPT}"
 
 # run the script
-./${SCRIPT}
+./${EXE_SCRIPT}
 
 # in case of an error we wouldn't have reached this line
 exit 0
