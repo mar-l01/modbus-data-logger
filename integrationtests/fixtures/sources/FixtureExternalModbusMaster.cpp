391b245e0d930b57e55bb6a5d53b8bc63594b506
Markus2101 <M.Loipfinger@hotmail.de>
2020-05-15 15:55:41 +0200

    feature/integration_tests_setup (#11)
    * started first integrationtest (introduced 2 fixtures (master and slave))
    
    * added python script to run integrationtests
    
    * added integrationtests to ci-environment (github action workflow)

---
+++ integrationtests/fixtures/sources/FixtureExternalModbusMaster.cpp
@@ -0,0 +1,55 @@
+#include "integrationtests/fixtures/includes/FixtureExternalModbusMaster.hpp"
+
+#include "integrationtests/fixtures/includes/TestConstants.hpp"
+
+#include "gtest/gtest.h"
+
+namespace Fixture {
+
+FixtureExternalModbusMaster::FixtureExternalModbusMaster() {}
+
+void FixtureExternalModbusMaster::setUp()
+{
+    // clang-format off
+    m_modbusContext = std::move(std::unique_ptr<modbus_t, std::function<void(modbus_t*)>>(
+        modbus_new_tcp(FixtureTestConstants::MODBUS_IP_ADDRESS, FixtureTestConstants::MODBUS_PORT),
+            [this](modbus_t* mbCtx) {
+                // use custom-deleter provided by libmodbus
+                modbus_free(mbCtx);
+            }));
+    // clang-format on
+
+    // make sure context is not null
+    ASSERT_NE(m_modbusContext, nullptr);
+
+    // connect
+    auto ec = modbus_connect(m_modbusContext.get());
+
+    // make sure connection is set up
+    ASSERT_NE(ec, -1);
+}
+
+void FixtureExternalModbusMaster::checkWriteReadModbusRequest()
+{
+    std::vector<uint16_t> readHoldingRegisters(FixtureTestConstants::MODBUS_NUMBER_HOLDING_REGISTERS);
+
+    // send Modbus request writing a single holding register...
+    auto rc = modbus_write_register(m_modbusContext.get(), FixtureTestConstants::MODBUS_START_ADDRESS_HOLDING_REGISTERS,
+                                    0x1234);
+    EXPECT_EQ(rc, 1);
+
+    // ...which is again read now
+    rc = modbus_read_registers(m_modbusContext.get(), FixtureTestConstants::MODBUS_START_ADDRESS_HOLDING_REGISTERS, 1,
+                               readHoldingRegisters.data());
+    EXPECT_EQ(rc, 1);
+
+    // test for equality
+    EXPECT_EQ(readHoldingRegisters[FixtureTestConstants::MODBUS_START_ADDRESS_HOLDING_REGISTERS], 0x1234);
+}
+
+void FixtureExternalModbusMaster::tearDown()
+{
+    modbus_close(m_modbusContext.get());
+}
+
+}
---
+++ integrationtests/fixtures/sources/FixtureModbusSlaveController.cpp
@@ -0,0 +1,63 @@
+#include "integrationtests/fixtures/includes/FixtureModbusSlaveController.hpp"
+
+#include "domain/framework/includes/LibModbusSlave.hpp"
+#include "integrationtests/fixtures/includes/TestConstants.hpp"
+
+#include "gtest/gtest.h"
+
+namespace Fixture {
+
+using namespace Gateway;
+
+FixtureModbusSlaveController::FixtureModbusSlaveController()
+    : m_modbusSlave(nullptr)
+    , m_socket(-1)
+{}
+
+void FixtureModbusSlaveController::setUp(const int nbIter)
+{
+    m_modbusSlave = std::make_unique<Framework::LibModbusSlave>();
+    ASSERT_NE(m_modbusSlave, nullptr);
+    m_modbusSlave->setModbusDataMapping(FixtureTestConstants::MODBUS_DATA_MAPPING);
+
+    // let slave run on localhost:5002 accepting one connection
+    m_modbusSlave->bind(FixtureTestConstants::MODBUS_IP_ADDRESS, FixtureTestConstants::MODBUS_PORT);
+    m_socket = m_modbusSlave->listen(1);
+    ASSERT_NE(m_socket, -1);
+    m_modbusSlave->accept(m_socket);
+
+    // run infinite Modbus slave loop
+    runRequestResponseLoop(nbIter);
+
+    // connection is closed afterwards
+    m_modbusSlave->close();
+}
+
+void FixtureModbusSlaveController::runRequestResponseLoop(const int nbIter)
+{
+    int reqLen = 0;
+    auto modbusRequest = std::vector<uint8_t>(ModbusConstants::MODBUS_TCP_REQUEST_LENGTH_MAX);
+
+    // infinite request loop
+    for (int i = 0; i < nbIter; ++i) {
+        do {
+            reqLen = m_modbusSlave->receive(modbusRequest);
+        } while (reqLen == 0); // 0:= indication request ignored
+
+        // error in receiving request
+        if (reqLen == -1) {
+            FAIL();
+            break;
+        }
+
+        // TODO(Markus2101, 14.05.2020): currently reply() argument is not required
+        auto modbusResponse = std::vector<uint8_t>(ModbusConstants::MODBUS_TCP_REQUEST_LENGTH_MAX);
+        reqLen = m_modbusSlave->reply(modbusResponse);
+        if (reqLen == -1) {
+            FAIL();
+            break;
+        }
+    }
+}
+
+}
