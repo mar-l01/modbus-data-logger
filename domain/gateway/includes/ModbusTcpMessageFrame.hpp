6c230692136df66414d226e87a11e915f72535b2
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-05-22 11:04:29 +0200

    added getter for dynamic parts of Modbus request/response

--- domain/gateway/includes/ModbusTcpMessageFrame.hpp
+++ domain/gateway/includes/ModbusTcpMessageFrame.hpp
@@ -2,7 +2,13 @@
 #include <iostream>
 #include <vector>
 
+
 namespace Gateway {
+
+namespace ModbusByteOffset {
+constexpr const int START_BYTE_READ_VALUES = 1;
+constexpr const int START_BYTE_WRITE_VALUES = 5;
+}
 
 class ModbusTcpMessageFrame
 {
@@ -16,11 +22,25 @@ public:
     uint8_t functionCode;
     std::vector<uint8_t> dataBytes;
 
+    uint16_t getStartAddress() const;                              // w, req/res and r, req
+    uint16_t getNumberOfValuesToReadOrWrite() const;               // r/w, req/res
+    uint16_t getSingleValueToWrite() const;                        // w, req/res
+    uint8_t getNumberOfBytesOfValuesToWrite() const;               // w, req
+    uint8_t getNumberOfBytesOfReadValues() const;                  // r, res
+    std::vector<uint8_t> getCoilValuesToWrite() const;             // w, req
+    std::vector<uint16_t> getHoldingRegisterValuesToWrite() const; // w, req
+    std::vector<uint8_t> getReadBitValues() const;                 // r, res
+    std::vector<uint16_t> getReadRegisterValues() const;           // r, res
+
     std::vector<uint8_t> asByteVector();
 
     friend std::ostream& operator<<(std::ostream& os, const ModbusTcpMessageFrame& mbMsgFrame);
 
     static ModbusTcpMessageFrame fromByteVector(const std::vector<uint8_t>& byteVector);
+
+private:
+    std::vector<uint8_t> extractBitValues(int startByte) const;
+    std::vector<uint16_t> extractRegisterValues(int startByte) const;
 };
 
 }
--- domain/gateway/sources/ModbusTcpMessageFrame.cpp
+++ domain/gateway/sources/ModbusTcpMessageFrame.cpp
@@ -6,6 +6,65 @@ namespace Gateway {
 
 ModbusTcpMessageFrame::ModbusTcpMessageFrame() {}
 
+uint16_t ModbusTcpMessageFrame::getStartAddress() const
+{
+    // allowed FCs: all
+    // start address := bytes 0 and 1 of data-bytes
+    return static_cast<uint16_t>(dataBytes[0] << 8) + dataBytes[1];
+}
+
+uint16_t ModbusTcpMessageFrame::getNumberOfValuesToReadOrWrite() const
+{
+    // allowed FCs: 0x01, 0x02, 0x03, 0x04, 0x0f, 0x10
+    // number of values to read/write := bytes 2 and 3 of data-bytes
+    return static_cast<uint16_t>(dataBytes[2] << 8) + dataBytes[3];
+}
+
+uint16_t ModbusTcpMessageFrame::getSingleValueToWrite() const
+{
+    // allowed FCs: 0x05, 0x06
+    // single value to write (coil or holding register) := bytes 2 and 3 of data-bytes
+    return static_cast<uint16_t>(dataBytes[2] << 8) + dataBytes[3];
+}
+
+uint8_t ModbusTcpMessageFrame::getNumberOfBytesOfValuesToWrite() const
+{
+    // allowed FCs: 0x0f, 0x10
+    // number of bytes to write (multiple coils or registers) := byte 4 of data-bytes
+    return dataBytes[4];
+}
+
+std::vector<uint8_t> ModbusTcpMessageFrame::getCoilValuesToWrite() const
+{
+    // allowed FC: 0x0f (start byte 5)
+    return extractBitValues(ModbusByteOffset::START_BYTE_WRITE_VALUES);
+}
+
+std::vector<uint16_t> ModbusTcpMessageFrame::getHoldingRegisterValuesToWrite() const
+{
+    // allowed FC: 0x10 (start byte 5)
+    return extractRegisterValues(ModbusByteOffset::START_BYTE_WRITE_VALUES);
+}
+
+uint8_t ModbusTcpMessageFrame::getNumberOfBytesOfReadValues() const
+{
+    // allowed FCs: 0x01, 0x02, 0x03, 0x04
+    // number of bytes to read (multiple bits or registers) := byte 0 of data-bytes
+    return dataBytes[0];
+}
+
+std::vector<uint8_t> ModbusTcpMessageFrame::getReadBitValues() const
+{
+    // allowed FC: 0x01, 0x02 (start byte 1)
+    return extractBitValues(ModbusByteOffset::START_BYTE_READ_VALUES);
+}
+
+std::vector<uint16_t> ModbusTcpMessageFrame::getReadRegisterValues() const
+{
+    // allowed FC: 0x03, 0x04 (start byte 1)
+    return extractRegisterValues(ModbusByteOffset::START_BYTE_READ_VALUES);
+}
+
 std::vector<uint8_t> ModbusTcpMessageFrame::asByteVector()
 {
     std::vector<uint8_t> convByteVector;
@@ -63,6 +122,47 @@ std::ostream& operator<<(std::ostream& os, const ModbusTcpMessageFrame& mbMsgFra
     }
 
     return os;
+}
+
+std::vector<uint8_t> ModbusTcpMessageFrame::extractBitValues(int startByte) const
+{
+    std::vector<uint8_t> bitValues;
+
+    // number of bits
+    auto nbBitValues = getNumberOfValuesToReadOrWrite();
+
+    // each bit represents one coil value, first requested coil is at LSB of first byte,
+    // MSBs of last byte are stuffed with 0 if they do not represent a coil
+    int currentBit = 0;
+    int currentByte = startByte;
+
+    for (int i = 0; i < nbBitValues; ++i) {
+        bitValues.emplace_back(((dataBytes[currentByte] >> currentBit) & 1) == 1 ? 1 : 0);
+
+        // jump to next byte and reset bit counter
+        if (currentBit == 7) {
+            currentBit = 0;
+            ++currentByte;
+        }
+    }
+
+    return bitValues;
+}
+
+std::vector<uint16_t> ModbusTcpMessageFrame::extractRegisterValues(int startByte) const
+{
+    std::vector<uint16_t> registerValues;
+
+    // number of registers
+    auto nbRegisterValues = getNumberOfValuesToReadOrWrite();
+
+    // each 2-bytes represent a single register
+    for (int i = 0; i < nbRegisterValues; ++i) {
+        registerValues.emplace_back(static_cast<uint16_t>(dataBytes[startByte + i] << 8) +
+                                    dataBytes[startByte + i + 1]);
+    }
+
+    return registerValues;
 }
 
 }
