e61dcd9bd81a75d3858b4fda3171208317fa48fe
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-05-24 16:22:10 +0200

    added LibModbusMaster instance

---
+++ domain/framework/testing/TestLibModbusMaster.cpp
@@ -0,0 +1,16 @@
+#include "domain/framework/includes/LibModbusMaster.hpp"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+using namespace Framework;
+using namespace Gateway;
+
+TEST(TestLibModbusMaster, ctor)
+{
+    EXPECT_NO_THROW(LibModbusMaster tstObj);
+}
+
+}
