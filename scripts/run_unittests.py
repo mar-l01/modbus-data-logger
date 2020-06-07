d5ecb314b6c685754d5dcb7ab4d3e3380e3be297
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-06-07 18:13:07 +0200

    print test-result in the end

--- scripts/run_unittests.py
+++ scripts/run_unittests.py
@@ -2,6 +2,10 @@ import os
 import subprocess
 import sys
 
+# colored test-results
+FAILED = "\033[91mFAILED\033[00m"
+PASSED = "\033[92mPASSED\033[00m"
+
 # get path of this script
 file_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -17,9 +21,18 @@ if not os.path.isdir(build_dir):
 return_codes = []
 
 # run unittests
-return_codes.append(subprocess.call(build_dir + "/framework_test"))
-return_codes.append(subprocess.call(build_dir + "/gateway_test"))
-return_codes.append(subprocess.call(build_dir + "/entity_test"))
+rc = subprocess.call(build_dir + "/framework_test")
+return_codes.append((rc, "[Framework] --> {}".format(FAILED if rc == 1 else PASSED)))
+rc = subprocess.call(build_dir + "/gateway_test")
+return_codes.append((rc, "[Gateway]   --> {}".format(FAILED if rc == 1 else PASSED)))
+rc = subprocess.call(build_dir + "/entity_test")
+return_codes.append((rc, "[Entity]    --> {}".format(FAILED if rc == 1 else PASSED)))
+
+# print summarized information about all unittests at the end
+print("\n==========================")
+for test_rc in return_codes:
+    print("= {} =".format(test_rc[1]))
+print("==========================")
 
 # exit script depending on test-result with error or not
 exit_code = 1 if 1 in return_codes else 0
