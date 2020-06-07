f5ff2ffaefe6754406942e81e93d21ccab10096b
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-06-07 17:54:13 +0200

    fixed return-code issue

--- scripts/run_unittests.py
+++ scripts/run_unittests.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 # get path of this script
 file_path = os.path.dirname(os.path.realpath(__file__))
@@ -10,12 +11,16 @@ build_dir = file_path + "/../builds/build_native"
 # check if path exists
 if not os.path.isdir(build_dir):
     print("Build directory '" + build_dir + "does not exist.")
-    exit(1)
+    sys.exit(1)
+
+# holds the return-code of each unit-testsuite
+return_codes = []
 
 # run unittests
-return_code = subprocess.call(build_dir + "/framework_test")
-return_code = subprocess.call(build_dir + "/gateway_test")
-return_code = subprocess.call(build_dir + "/entity_test")
+return_codes.append(subprocess.call(build_dir + "/framework_test"))
+return_codes.append(subprocess.call(build_dir + "/gateway_test"))
+return_codes.append(subprocess.call(build_dir + "/entity_test"))
 
 # exit script depending on test-result with error or not
-exit(return_code)
+exit_code = 1 if 1 in return_codes else 0
+sys.exit(exit_code)
