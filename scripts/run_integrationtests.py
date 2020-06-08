d0db1d4bb6b198724aa306697e7aeb0ab0df6767
Markus2101 <M.Loipfinger@hotmail.de>
2020-06-08 14:05:05 +0200

    bugfix/issue-39_summarize_unit-tests (#45)
    * fixed return-code issue
    
    * print test-result in the end
    
    * use sys-module for exit() in run_integrationtests.py, too

--- scripts/run_integrationtests.py
+++ scripts/run_integrationtests.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 # get path of this script
 file_path = os.path.dirname(os.path.realpath(__file__))
@@ -10,10 +11,10 @@ build_dir = file_path + "/../builds/build_native"
 # check if path exists
 if not os.path.isdir(build_dir):
     print("Build directory '" + build_dir + "does not exist.")
-    exit(1)
+    sys.exit(1)
 
 # run integrationtests
 return_code = subprocess.call(build_dir + "/integrationtest")
 
 # exit script depending on test-result with error or not
-exit(return_code)
+sys.exit(return_code)
