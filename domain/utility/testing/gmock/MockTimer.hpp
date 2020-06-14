dc250ea5825f482f5c49f1b922477137792a539a
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-06-14 14:04:08 +0200

    extend mock with previously implemented functions

--- domain/utility/testing/gmock/MockTimer.hpp
+++ domain/utility/testing/gmock/MockTimer.hpp
@@ -8,4 +8,6 @@ class MockTimer : public Utility::Timer
 {
 public:
     MOCK_METHOD(void, callOnTimeout, (const int, const std::function<void()>&), (override));
+    MOCK_METHOD(void, restart, (), (override));
+    MOCK_METHOD(void, stop, (), (override));
 };
