15db898f3322fc78f4dd94a20eb7bbce08b6779f
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-06-15 16:20:07 +0200

    define configuration file (json), add interface methods

--- domain/framework/interfaces/ConfigurationFileReader.hpp
+++ domain/framework/interfaces/ConfigurationFileReader.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "domain/entity/includes/ModbusDataMapping.hpp"
+
 #include <string>
 
 namespace Framework {
@@ -7,6 +9,21 @@ namespace Framework {
 class ConfigurationFileReader
 {
     virtual void setConfigurationFilePath(const std::string& path) = 0;
+
+    // internal Modbus slave configuration
+    virtual std::string getIpAddressInternalModbusSlave() const = 0;
+    virtual int getPortInternalModbusSlave() const = 0;
+
+    // external Modbus slave configuration
+    virtual std::string getIpAddressExternalModbusSlave() const = 0;
+    virtual int getPortExternalModbusSlave() const = 0;
+
+    // Modbus data mapping
+    virtual Entity::ModbusDataMapping getModbusDataMapping() const = 0;
+
+    // common configuration
+    virtual int getModbusTimeout() const = 0;
+    virtual int getApplicationTimeout() const = 0;
 };
 
 }
