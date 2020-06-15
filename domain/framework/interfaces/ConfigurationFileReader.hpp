664be0eabc69adbc54343f17151aeffe6de8b42e
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-06-15 17:01:43 +0200

    remove most methods of ConfigurationFileReader, return entity ModbusConfiguration object

--- domain/framework/interfaces/ConfigurationFileReader.hpp
+++ domain/framework/interfaces/ConfigurationFileReader.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "domain/entity/includes/ModbusDataMapping.hpp"
+#include "domain/entity/includes/ModbusConfiguration.hpp"
 
 #include <string>
 
@@ -8,22 +8,9 @@ namespace Framework {
 
 class ConfigurationFileReader
 {
-    virtual void setConfigurationFilePath(const std::string& path) = 0;
+    virtual void readConfigurationFile(const std::string& path) = 0;
 
-    // internal Modbus slave configuration
-    virtual std::string getIpAddressInternalModbusSlave() const = 0;
-    virtual int getPortInternalModbusSlave() const = 0;
-
-    // external Modbus slave configuration
-    virtual std::string getIpAddressExternalModbusSlave() const = 0;
-    virtual int getPortExternalModbusSlave() const = 0;
-
-    // Modbus data mapping
-    virtual Entity::ModbusDataMapping getModbusDataMapping() const = 0;
-
-    // common configuration
-    virtual int getModbusTimeout() const = 0;
-    virtual int getApplicationTimeout() const = 0;
+    virtual Entity::ModbusConfiguration getModbusConfiguration() const = 0;
 };
 
 }
--- domain/framework/sources/JsonFileReader.cpp
+++ domain/framework/sources/JsonFileReader.cpp
@@ -4,43 +4,14 @@ namespace Framework {
 
 JsonFileReader::JsonFileReader() {}
 
-void JsonFileReader::setConfigurationFilePath(const std::string& path)
+void JsonFileReader::readConfigurationFile(const std::string& path)
 {
     return;
 }
-std::string JsonFileReader::getIpAddressInternalModbusSlave() const
-{
-    return "";
-}
 
-int JsonFileReader::getPortInternalModbusSlave() const
+Entity::ModbusConfiguration JsonFileReader::getModbusConfiguration() const
 {
-    return 0;
-}
-
-std::string JsonFileReader::getIpAddressExternalModbusSlave() const
-{
-    return "";
-}
-
-int JsonFileReader::getPortExternalModbusSlave() const
-{
-    return 0;
-}
-
-Entity::ModbusDataMapping JsonFileReader::getModbusDataMapping() const
-{
-    return Entity::ModbusDataMapping();
-}
-
-int JsonFileReader::getModbusTimeout() const
-{
-    return 0;
-}
-
-int JsonFileReader::getApplicationTimeout() const
-{
-    return 0;
+    return Entity::ModbusConfiguration();
 }
 
 }
