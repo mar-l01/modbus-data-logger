d534b93dd44869fc3a6c2c6537bf013e33009426
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-06-20 12:17:26 +0200

    add TODO to stop script

--- service/stop_mbdl_service.sh
+++ service/stop_mbdl_service.sh
@@ -1,2 +1,4 @@
 # send a SIGINT (:= 2)
+# TODO(Markus2101, 20.06.2020): needs to be tested in real environment
+#       Check whether SIGKILL (-9) should be used to stop the application completely
 killall -2 modbus_data_logger
