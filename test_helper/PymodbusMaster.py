c16f685dd2c1473fffb933552e170d8f8fb3f069
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-06-22 18:21:13 +0200

    implemented Modbus master and slave for HW test

---
+++ test_helper/PymodbusMaster.py
@@ -0,0 +1,26 @@
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+
+def run_modbus_master():
+    client = ModbusClient('localhost', port=5020)
+    client.connect()
+
+    holding_reg_addr = 1
+    holding_reg_val = 10
+    print("Writing value {} to holding register at address {}".format(holding_reg_addr, holding_reg_val))
+    req = client.write_register(holding_reg_addr, holding_reg_val)
+    res = client.read_holding_registers(holding_reg_addr, 1)
+    assert(not req.isError())
+    assert(res.registers[0] == holding_reg_val)
+
+    holding_reg_vals = [10,20,30,40,50]
+    print("Writing values {} to holding registers starting at address {}".format(holding_reg_addr, holding_reg_val))
+    req = client.write_registers(holding_reg_addr, holding_reg_vals)
+    res = client.read_holding_registers(holding_reg_addr, len(holding_reg_vals))
+    assert(not req.isError())
+    assert(res.registers == holding_reg_vals)
+
+    client.close()
+
+
+if __name__ == "__main__":
+    run_modbus_master()
---
+++ test_helper/PymodbusSlave.py
@@ -0,0 +1,20 @@
+from pymodbus.server.sync import StartTcpServer
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSlaveContext, ModbusServerContext
+
+
+def run_server():
+    mb_mapping = ModbusSlaveContext(
+        di=ModbusSequentialDataBlock(0, [0]*10),
+        co=ModbusSequentialDataBlock(0, [0]*10),
+        hr=ModbusSequentialDataBlock(0, [0]*10),
+        ir=ModbusSequentialDataBlock(0, [0]*10))
+
+    context = ModbusServerContext(slaves=mb_mapping, single=True)
+
+    StartTcpServer(context, address=("localhost", 5002))
+
+if __name__ == "__main__":
+    try:
+        run_server()
+    except KeyboardInterrupt:
+        print("CTRL-C: Closing Modbus slave")
