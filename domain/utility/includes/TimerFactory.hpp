320472e456497440a349a6ad03fa4d6603cecbab
Markus2101 <M.Loipfinger@hotmail.de>
2020-07-27 13:23:49 +0200

    feature/issue-52_add_timer_factory (#76)
    * added header and source for TimerFactory, included it into CMakeLists.txt
    
    * add extra method to timer instance which allows for setting the loop frequency
    
    * replace Timer instantiation with timer factory call
    
    * add unittest for TimerFactory, moved default method argument from source to header file

---
+++ domain/utility/includes/TimerFactory.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "domain/utility/interfaces/Timer.hpp"
+
+#include <memory>
+
+namespace Utility {
+
+class TimerFactory
+{
+public:
+    static std::shared_ptr<Timer> createLoopTimer(const int frequencyInMs = 1);
+};
+
+}
---
+++ domain/utility/sources/TimerFactory.cpp
@@ -0,0 +1,15 @@
+#include "domain/utility/includes/TimerFactory.hpp"
+
+#include "domain/utility/includes/TimerImpl.hpp"
+
+namespace Utility {
+
+std::shared_ptr<Timer> TimerFactory::createLoopTimer(const int frequencyInMs)
+{
+    auto timerInstance = std::make_shared<TimerImpl>();
+    timerInstance->setFrequencyInMs(frequencyInMs);
+
+    return timerInstance;
+}
+
+}
