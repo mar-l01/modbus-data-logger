f918f1afe8f32739b991bd364ca20257df5159e7
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-08-08 15:15:07 +0200

    added documentation to SpdlogFileController

--- domain/framework/includes/SpdlogFileController.hpp
+++ domain/framework/includes/SpdlogFileController.hpp
@@ -8,14 +8,43 @@
 
 namespace Framework {
 
+/**
+ * @brief This class represents a kind of controller for a file-logger, implemented with the 'spdlog'-framework.
+ * It derives from @ref FileLoggerController, which internally controls the logging of messages. It provides
+ * methods to
+ * - start logging,
+ * - stop logging,
+ * - log actual data and
+ * - configure it.
+ *
+ * For further information about the 'spdlog'-framwork see: https://github.com/gabime/spdlog
+ */
 class SpdlogFileController : public Logging::FileLoggerController
 {
 public:
+    /**
+     * @brief Construct a new SpdlogFileController object.
+     */
     SpdlogFileController();
 
+    /**
+     * @see FileLoggerController::startLogger
+     */
     void startLogger() override;
+
+    /**
+     * @see FileLoggerController::stopLogger
+     */
     void stopLogger() override;
+
+    /**
+     * @see FileLoggerController::setLogConfiguration
+     */
     void setLogConfiguration(const Entity::ModbusLoggerConfiguration& mbLogConfig) override;
+
+    /**
+     * @see FileLoggerController::logModbusData
+     */
     void logModbusData(const std::shared_ptr<Entity::ModbusDataLog>& mbModbusData) override;
 
 private:
