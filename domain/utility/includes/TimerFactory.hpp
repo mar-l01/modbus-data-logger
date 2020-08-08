42ca5959b1e8f2a03d9c9ec29b687300ea4694b5
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-08-08 17:32:20 +0200

    added documentation to TimerFactory

--- domain/utility/includes/TimerFactory.hpp
+++ domain/utility/includes/TimerFactory.hpp
@@ -6,9 +6,18 @@
 
 namespace Utility {
 
+/**
+ * @brief Factory to create a @ref Timer instance.
+ */
 class TimerFactory
 {
 public:
+    /**
+     * @brief Create a LoopTimer object.
+     *
+     * @param frequencyInMs The frequency in which the loop will sleep, provided in milliseconds.
+     * @return A shared-ptr instance of type @ref Timer.
+     */
     static std::shared_ptr<Timer> createLoopTimer(const int frequencyInMs = 1);
 };
 
