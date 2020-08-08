dcaba8fe01d97c10cb7319ce3395eacb3fa43493
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-08-08 14:40:44 +0200

    added documentation to ModbusDataLog

--- domain/entity/interfaces/ModbusDataLog.hpp
+++ domain/entity/interfaces/ModbusDataLog.hpp
@@ -4,9 +4,17 @@
 
 namespace Entity {
 
+/**
+ * @brief This interface provides a single method which is used for logging Modbus messages
+ */
 class ModbusDataLog
 {
 public:
+    /**
+     * @brief This methods needs to be implemented and will be used to log a Modbus message (request or response)
+     *
+     * @return a std::string representation of the Modbus message
+     */
     virtual std::string convertToLogString() const = 0;
 };
 
