1911c9e73e7bf4a016a748761ef59bb5f62e4fa1
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-08-08 16:13:04 +0200

    added documentation to ModbusRequestController

--- domain/gateway/interfaces/ModbusRequestController.hpp
+++ domain/gateway/interfaces/ModbusRequestController.hpp
@@ -7,9 +7,20 @@
 
 namespace Gateway {
 
+/**
+ * @brief This interface represents a controller to forward Modbus requests to an external Modbus slave
+ * and wait for a response of it.
+ */
 class ModbusRequestController
 {
 public:
+    /**
+     * @brief Forward provided Modbus request to a Modbus slave and wait for its response.
+     *
+     * @param mbRequest The Modbus TCP request (see @ref ModbusTcpRequest) which should be forwarded to the Modbus
+     * slave.
+     * @return The Modbus TCP response (see @ref ModbusTcpResponse) received from the Modbus slave.
+     */
     virtual std::shared_ptr<Entity::ModbusTcpResponse> forwardModbusRequestAndWaitForResponse(
       std::shared_ptr<Entity::ModbusTcpRequest>& mbRequest) = 0;
 };
