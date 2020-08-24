d3b098973c0ba7a90609c4e8ddbc5295570db4b3
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-08-24 10:18:00 +0200

    added implementation to interface methods

--- domain/application/includes/ModbusDataLoggerFacadeImpl.hpp
+++ domain/application/includes/ModbusDataLoggerFacadeImpl.hpp
@@ -3,6 +3,17 @@
 #include "application/interfaces/ModbusDataLoggerFacade.hpp"
 #include <string>
 
+namespace Gateway {
+// forward declarations
+class ModbusSlaveController;
+class ModbusMasterController;
+}
+
+namespace Logging {
+// forward declaration
+class ModbusDataLogger;
+}
+
 namespace Application {
 
 class ModbusDataLoggerFacadeImpl : public ModbusDataLoggerFacade
@@ -29,6 +40,13 @@ public:
      * @see ModbusDataLoggerFacade::stopLogger
      */
     void stopLogger() override;
+
+private:
+    std::unique_ptr<Gateway::ModbusSlaveController> m_mbSlaveController;
+    std::shared_ptr<Gateway::ModbusMasterController> m_mbMasterController;
+    std::shared_ptr<Logging::ModbusDataLogger> m_dataLogger;
+
+    void createModbusGateway(const std::string& mbConfigFilepath);
 };
 
 }
