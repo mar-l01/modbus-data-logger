f706413240648d7a7d749287f4b301e8fb333e19
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-08-24 09:32:36 +0200

    add source file

---
+++ domain/application/sources/ModbusDataLoggerFacadeImpl.cpp
@@ -0,0 +1,15 @@
+#include "domain/application/includes/ModbusDataLoggerFacadeImpl.hpp"
+
+namespace Application {
+
+ModbusDataLoggerFacadeImpl::ModbusDataLoggerFacadeImpl(const std::string& mbConfigFilepath) {}
+
+void ModbusDataLoggerFacadeImpl::startModbusCommunication() {}
+
+void ModbusDataLoggerFacadeImpl::stopModbusCommunication() {}
+
+void ModbusDataLoggerFacadeImpl::startLogger() {}
+
+void ModbusDataLoggerFacadeImpl::stopLogger() {}
+
+}
