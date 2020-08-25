c0509afafb02297fda372b7a819f9276fc013966
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-08-25 12:06:54 +0200

    add ModbusSlaveController mock

---
+++ domain/gateway/testing/gmock/MockModbusSlaveController.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "domain/gateway/interfaces/ModbusSlaveController.hpp"
+
+#include "gmock/gmock.h"
+
+class MockModbusSlaveController : public Gateway::ModbusSlaveController
+{
+public:
+    MOCK_METHOD(void, waitForIncomingConnection, (), (override));
+    MOCK_METHOD(void, run, (), (override));
+    MOCK_METHOD(void, disconnect, (), (override));
+};
