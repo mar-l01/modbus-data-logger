0de7b73ac5dabb90e0115f36ee9189a3a6e618e2
mar-l01 <M.Loipfinger@hotmail.de>
2020-08-27 12:27:52 +0200

    feature/issue-100-(III)_add_basic_factory_class_for_ModbusDataLoggerFacade (#108)
    * add header and source of factory class
    
    * added unittest for successful creation of facade

---
+++ domain/application/includes/ModbusDataLoggerFacadeFactory.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "domain/application/interfaces/ModbusDataLoggerFacade.hpp"
+#include "domain/framework/includes/FileLoggerControllerFactory.hpp"
+#include "domain/framework/includes/FileReaderFactory.hpp"
+#include "domain/gateway/includes/ModbusComponentsFactory.hpp"
+
+#include <memory>
+#include <string>
+
+namespace Application {
+
+/**
+ * @brief This datastructure holds all necessary dependencies to frameworks which implement the provided interfaces
+ * using Dependency Injection.
+ */
+struct FrameworkDependencies
+{
+    FrameworkDependencies(Framework::LoggingFramework logFramework, Framework::FileReaderFramework fReaderFramework,
+                          Gateway::ModbusComponentsFramework mbMasterFramework,
+                          Gateway::ModbusComponentsFramework mbSlaveFramework)
+        : loggingFramework(logFramework)
+        , fileReaderFramework(fReaderFramework)
+        , modbusMasterFramework(mbMasterFramework)
+        , modbusSlaveFramework(mbSlaveFramework)
+    {}
+
+    Framework::LoggingFramework loggingFramework;
+    Framework::FileReaderFramework fileReaderFramework;
+    Gateway::ModbusComponentsFramework modbusMasterFramework;
+    Gateway::ModbusComponentsFramework modbusSlaveFramework;
+};
+
+/**
+ * @brief Factory to create a @ref ModbusDataLoggerFacade which holds all necessary dependencies to run the application.
+ */
+class ModbusDataLoggerFacadeFactory
+{
+public:
+    /**
+     * @brief Create a ModbusDataLoggerFacade which has all components to run the Modbus data logger application.
+     *
+     * @param mbConfigFilepath Filepath to the Modbus configuration file
+     * @param frameworkDependencies Struct holding all required framework dependencies
+     * @return A shared-ptr instance of type @ref ModbusDataLoggerFacade.
+     */
+    static std::shared_ptr<ModbusDataLoggerFacade> createModbusDataLoggerFacade(
+      const std::string& mbConfigFilepath, const FrameworkDependencies& frameworkDependencies);
+};
+
+}
