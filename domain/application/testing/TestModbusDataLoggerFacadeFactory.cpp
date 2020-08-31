82b5bd7ed021799cf1b8c8978c7c5aa679ea42d5
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-08-31 18:20:58 +0200

    add unittests for failing object initialization

--- domain/application/testing/TestModbusDataLoggerFacadeFactory.cpp
+++ domain/application/testing/TestModbusDataLoggerFacadeFactory.cpp
@@ -19,4 +19,40 @@ TEST(TestModbusDataLoggerFacadeFactory, createModbusDataLoggerFacadeFactoryRetur
     EXPECT_NE(mbDataLoggerFacade, nullptr);
 }
 
+TEST(TestModbusDataLoggerFacadeFactory, createModbusDataLoggerFacadeFactory_loggerNullptr)
+{
+    // pre-condition: framework not implemented
+    auto frameworkDependenciesError = frameworkDependencies;
+    frameworkDependenciesError.loggingFramework = Framework::LoggingFramework::OTHER_LOGGING_FRAMEWORK;
+
+    EXPECT_ANY_THROW(ModbusDataLoggerFacadeFactory::createModbusDataLoggerFacade(filepath, frameworkDependenciesError));
+}
+
+TEST(TestModbusDataLoggerFacadeFactory, createModbusDataLoggerFacadeFactory_fileReaderNullptr)
+{
+    // pre-condition: framework not implemented
+    auto frameworkDependenciesError = frameworkDependencies;
+    frameworkDependenciesError.fileReaderFramework = Framework::FileReaderFramework::OTHER_READER_FRAMEWORK;
+
+    EXPECT_ANY_THROW(ModbusDataLoggerFacadeFactory::createModbusDataLoggerFacade(filepath, frameworkDependenciesError));
+}
+
+TEST(TestModbusDataLoggerFacadeFactory, createModbusDataLoggerFacadeFactory_mbMasterNullptr)
+{
+    // pre-condition: framework not implemented
+    auto frameworkDependenciesError = frameworkDependencies;
+    frameworkDependenciesError.modbusMasterFramework = Gateway::ModbusComponentsFramework::OTHER_MODBUS_FRAMEWORK;
+
+    EXPECT_ANY_THROW(ModbusDataLoggerFacadeFactory::createModbusDataLoggerFacade(filepath, frameworkDependenciesError));
+}
+
+TEST(TestModbusDataLoggerFacadeFactory, createModbusDataLoggerFacadeFactory_mbSlaveNullptr)
+{
+    // pre-condition: framework not implemented
+    auto frameworkDependenciesError = frameworkDependencies;
+    frameworkDependenciesError.modbusSlaveFramework = Gateway::ModbusComponentsFramework::OTHER_MODBUS_FRAMEWORK;
+
+    EXPECT_ANY_THROW(ModbusDataLoggerFacadeFactory::createModbusDataLoggerFacade(filepath, frameworkDependenciesError));
+}
+
 }
