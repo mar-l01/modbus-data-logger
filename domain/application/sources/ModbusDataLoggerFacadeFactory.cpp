bbdf89a8744ed7ad4183599295895c181934e5ac
mar-l01 <M.Loipfinger@hotmail.de>
2020-09-01 14:20:29 +0200

    feature/issue-100-(IV)_assert_no_nullptr_objects_in_facade (#109)
    * throw exception if object is nullptr in factory method
    
    * add unittests for failing object initialization

--- domain/application/sources/ModbusDataLoggerFacadeFactory.cpp
+++ domain/application/sources/ModbusDataLoggerFacadeFactory.cpp
@@ -9,22 +9,32 @@
 #include "domain/utility/interfaces/Timer.hpp"
 
 #include "spdlog/spdlog.h"
+#include <string>
 
 namespace Application {
 
 std::shared_ptr<ModbusDataLoggerFacade> ModbusDataLoggerFacadeFactory::createModbusDataLoggerFacade(
   const std::string& mbConfigFilepath, const FrameworkDependencies& frameworkDependencies)
 {
+    // helper to make sure, generated objects are not nullptr
+    auto assertNotNullptr = [](const auto& interfacePointer, int line) {
+        if (interfacePointer == nullptr) {
+            throw std::string("[Line: " + std::to_string(line) + "]: Object is nullptr: Failed to create object!");
+        }
+    };
     // read in Modbus configuration
     auto fileReader = Framework::FileReaderFactory::createFileReader(frameworkDependencies.fileReaderFramework);
+    assertNotNullptr(fileReader, __LINE__);
     fileReader->readConfigurationFile(mbConfigFilepath);
     const auto& mbConfig = fileReader->getModbusConfiguration();
 
     // get Modbus master instance from factory
     auto mbMaster = Gateway::ModbusComponentsFactory::createModbusMaster(frameworkDependencies.modbusMasterFramework);
+    assertNotNullptr(mbMaster, __LINE__);
 
     // get Modbus slave instance from factory
     auto mbSlave = Gateway::ModbusComponentsFactory::createModbusSlave(frameworkDependencies.modbusSlaveFramework);
+    assertNotNullptr(mbSlave, __LINE__);
 
     // create Modbus master controller
     auto mbMasterController =
@@ -34,10 +44,13 @@ std::shared_ptr<ModbusDataLoggerFacade> ModbusDataLoggerFacadeFactory::createMod
     // create data logger
     auto fileLoggerController =
       Framework::FileLoggerControllerFactory::createFileLoggerController(frameworkDependencies.loggingFramework);
+    assertNotNullptr(fileLoggerController, __LINE__);
     auto dataLogger = std::make_shared<Logging::ModbusDataLogger>(fileLoggerController);
+    assertNotNullptr(dataLogger, __LINE__);
 
     // set up Modbus gateway
     auto mbGateway = std::make_shared<Gateway::ModbusGateway>(mbMasterController, dataLogger);
+    assertNotNullptr(mbGateway, __LINE__);
 
     // create timer instance with a loop frequency of 1 ms
     std::atomic_bool timeoutStop = false;
@@ -46,15 +59,18 @@ std::shared_ptr<ModbusDataLoggerFacade> ModbusDataLoggerFacadeFactory::createMod
         timeoutStop = true;
         SPDLOG_INFO("Timeout reached!");
     });
+    assertNotNullptr(timerInstance, __LINE__);
 
     // create Modbus slave controller and start it up
     auto mbSlaveController = std::make_shared<Gateway::ModbusSlaveControllerImpl>(
       mbSlave, mbGateway, timerInstance, mbConfig.dataMapping, mbConfig.ipAddrIntSlave, mbConfig.portIntSlave);
+    assertNotNullptr(mbSlaveController, __LINE__);
 
     // create facade (cast required components to their interface type)
     std::shared_ptr<ModbusDataLoggerFacade> mbDataLoggerFacade = std::make_shared<ModbusDataLoggerFacadeImpl>(
       std::dynamic_pointer_cast<Gateway::ModbusMasterController>(mbMasterController),
       std::dynamic_pointer_cast<Gateway::ModbusSlaveController>(mbSlaveController), dataLogger);
+    assertNotNullptr(mbDataLoggerFacade, __LINE__);
 
     return mbDataLoggerFacade;
 }
