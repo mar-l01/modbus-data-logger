edca7c5d80d87fb38bdf68dcd60380f9fa034054
Markus Loipfinger <m.loipfinger@hotmail.de>
2021-01-01 16:38:44 +0100

    provide the Modbus configuration file as argument to function call

--- gui/scripts/run_ui_native.py
+++ gui/scripts/run_ui_native.py
@@ -8,12 +8,15 @@ file_path = os.path.dirname(os.path.realpath(__file__))
 # get path to build-directory
 build_dir = file_path + "/../builds/build_native/"
 
+# get Modbus configuration file path
+mb_config_path = file_path + "/../../resources/mbdl_config.json"
+
 # check if path exists
 if not os.path.isdir(build_dir):
     print("Build directory '" + build_dir + "does not exist.")
     sys.exit(1)
 
 # start UI application
-subprocess.call(build_dir + "/ui_application")
+subprocess.call([build_dir + "/ui_application", mb_config_path])
 
 sys.exit(0)
--- gui/ui/application/GuiApplication.cpp
+++ gui/ui/application/GuiApplication.cpp
@@ -9,6 +9,12 @@
 
 int main(int argc, char* argv[])
 {
+    // TODO (mar-l01, 01.01.2021): Workaround to set the Modbus configuration file
+    if (argc <= 1) {
+        return 1;
+    }
+    const std::string mbConfigFile(argv[1]);
+
     // create application main window
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setApplicationName("Modbus Data-Logger");
@@ -17,12 +23,11 @@ int main(int argc, char* argv[])
     QQuickView view;
 
     // create facade
-    const auto configFile = "../resources/mbdl_config.json";
     const Application::FrameworkDependencies frameworkDependencies(
       Framework::LoggingFramework::SPDLOG, Framework::FileReaderFramework::NLOHMANN_JSON,
       Gateway::ModbusComponentsFramework::LIBMODBUS, Gateway::ModbusComponentsFramework::LIBMODBUS);
     const auto mbDataLoggerFacade =
-      Application::ModbusDataLoggerFacadeFactory::createModbusDataLoggerFacade(configFile, frameworkDependencies);
+      Application::ModbusDataLoggerFacadeFactory::createModbusDataLoggerFacade(mbConfigFile, frameworkDependencies);
 
     const auto rootContext = view.rootContext();
 
