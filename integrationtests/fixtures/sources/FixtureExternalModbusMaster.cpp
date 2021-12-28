e91652846dd6e5b042563545050ae9e8589ff9ec
Markus Loipfinger <m.loipfinger@hotmail.de>
2021-12-28 13:15:21 +0100

    re-do workarounds

--- integrationtests/fixtures/sources/FixtureExternalModbusMaster.cpp
+++ integrationtests/fixtures/sources/FixtureExternalModbusMaster.cpp
@@ -40,13 +40,6 @@ void FixtureExternalModbusMaster::setUp(bool expectConnectionFailure)
     } else {
         // make sure connection is set up
         ASSERT_NE(ec, -1);
-
-        // try to re-connect once after 100ms, if a connection couldn't have been established
-        if (ec == -1) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            auto ec = modbus_connect(m_modbusContext.get());
-            ASSERT_NE(ec, -1);
-        }
     }
 }
 
